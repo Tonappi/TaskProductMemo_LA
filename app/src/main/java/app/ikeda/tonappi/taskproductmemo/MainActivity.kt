37169badaeb144bba832403eaa73860933142d42
Tonappi <ikedatomona.lifeistech@gmail.com>
2022-09-07 13:09:32 +0900

    リスト表示の実装

--- app/src/main/java/app/ikeda/tonappi/taskproductmemo/MainActivity.kt
+++ app/src/main/java/app/ikeda/tonappi/taskproductmemo/MainActivity.kt
@@ -2,6 +2,8 @@ package app.ikeda.tonappi.taskproductmemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import app.ikeda.tonappi.taskproductmemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        //Adapterに渡す配列を作成
+        val data = arrayOf("test1","test2","test3","test4")
+
+        //xmlにて実装したListViewの取得
+        val listView = findViewById<ListView>(R.id.list_view)
+
+        //adapterを作成
+        val adapter = ArrayAdapter (this, android.R.layout.simple_list_item_1,data)
+
+        //adapterをlistViewに紐付ける
+        listView.adapter = adapter
+
+
+
     }
 }
