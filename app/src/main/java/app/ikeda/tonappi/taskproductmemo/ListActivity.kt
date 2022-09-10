db99e9227c3654a7e20b70b91c9fa43132353adc
Tonappi <ikedatomona.lifeistech@gmail.com>
2022-09-10 11:16:27 +0900

    データの永続化

--- app/src/main/java/app/ikeda/tonappi/taskproductmemo/ListActivity.kt
+++ app/src/main/java/app/ikeda/tonappi/taskproductmemo/ListActivity.kt
@@ -16,18 +16,25 @@ class ListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityListBinding
 
+    private lateinit var pref: SharedPreferences
+
+    //Adapterに渡す配列を作成
+    private var data = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityListBinding.inflate(layoutInflater).apply { setContentView(this.root) }
-
+        pref = getSharedPreferences("SharedPref",Context.MODE_PRIVATE)
 
 
         //xmlにて実装したListViewの取得
         val listView = findViewById<ListView>(R.id.list_view)
 
-        //Adapterに渡す配列を作成
-        var data = mutableListOf<String>("test1","test2","test3","test4")
+        //前回までのメモを呼び出し
+        val savedString = pref.getString("DataKey","NoData")
+        //文字列で保存したデータを配列にして加える
+        val savedList = savedString?.split(',')
+        data.add(savedList.toString())
 
         //MemoActivityで入力したメモの取り出し
         var memo = intent.getStringExtra("NEW_MEMO")
@@ -38,7 +45,7 @@ class ListActivity : AppCompatActivity() {
             data.add(memo.toString())
         }
 
-        Log.d("ListData",data.toString())
+
 
         //adapterを作成
         val adapter = ArrayAdapter (this, android.R.layout.simple_list_item_1,data)
@@ -46,37 +53,31 @@ class ListActivity : AppCompatActivity() {
         //adapterをlistViewに紐付ける
         listView.adapter = adapter
 
-
-        //add_buttonクリック時にMemoActivityへ画面遷移
+        //add_buttonクリック時
         binding.addButton.setOnClickListener{
-
+            //MemoActivityへ画面遷移
             val toMemoActivityIntent = Intent(this, MemoActivity::class.java)
             startActivity(toMemoActivityIntent)
+
         }
 
-    }
-    /*
-    // リストの保存
-    fun saveList (key:String, data:String){
-       val shardPreferences = this.getPreferences(Context.MODE_PRIVATE)
-        val shardPrefEditor = shardPreferences.edit()
 
-        val jsonArray = JSONArray(data)
-        shardPrefEditor.putString(key, jsonArray.toString())
-        shardPrefEditor.apply()
     }
 
-    // リストの読み込み
-    fun loadList(key: String) {
 
-        val shardPreferences = this.getPreferences(Context.MODE_PRIVATE)
 
-        val jsonArray = JSONArray(shardPreferences.getString(key, "[]"));
+    override fun onPause() {
+        super.onPause()
 
-        for (i in 0 until jsonArray.length()) {
-            Log.i("loadArrayList", "[$i] -> " + jsonArray.get(i))
-        }
-    }*/
+        /*リストを文字列に変換する
+        data.joinToString(separator = ",")
+        Log.d("ListData",data.toString())
+        //dataの書き込み
+        val editor = pref.edit()
+        editor.putString("DataKey", data.toString())
+        editor.apply()*/
+    }
+
 }
 
 private fun <E> MutableList<E>.add(index: E?, element: E) {
